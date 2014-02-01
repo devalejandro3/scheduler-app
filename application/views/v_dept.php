0e370ae4f65841407bf48d87d01c9f5a345f58aa
Julius DC Mendoza <juliusdcmendoza2@gmail.com>
2014-02-01 17:18:55 +0800

    added new files for department

---
+++ application/views/v_dept.php
@@ -0,0 +1,45 @@
+ <!DOCTYPE html>
+<html lang="en">
+	<head>
+		
+		<meta charset="utf-8">
+		<title> <?php echo $title ?> </title>
+		<link rel="stylesheet" type="text/css" href="/scheduler-app/resources/css/myStyles.css"/>
+		<link rel="stylesheet" type="text/css" href="/scheduler-app/resources/css/maintenance.css"/>
+		<script  src="/scheduler-app/resources/jquery/jquery-2.1.0.js"></script>
+		<script  src="/scheduler-app/resources/jquery/jquery-2.1.0.min.js"></script>
+		<script  src="/scheduler-app/resources/javascript/login.js"></script>
+		<script></script>
+	</head>
+	<body>
+
+		<div id="header">
+			<div class="header_inner">
+				
+				<div><?php $this->load->view( $header ) ?></div>
+				<div style="text-align:right;"><div>Welcome : <?php echo $row ?></div><a href="/scheduler-app">	 Logout</a></div>
+				
+			</div>
+		</div>
+
+		<div class="menu_bar">
+			<div>HOME | MENU2 | MENU3 </div>
+		</div>
+
+		<div class='content'> 
+
+			<div class='content_inner '  style="border:0px solid yellow;">
+
+			</div>		
+
+		</div>
+
+		<div class="footer">
+
+			<?php $this->load->view( $footer ) ?>
+		</div>
+
+		
+
+	</body>
+</html>
